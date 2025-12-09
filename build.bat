87b0ad1b3a66da0a96d724d5f3c22753e0d169d6
antonic901 <antonicnikola6@gmail.com>
2025-12-09 20:55:55 +0100

    [CI/CD] added: workflow for building and releasing skin

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
 @echo off
+SET ADDON_NAME_ID=skin.confluence
+
 ECHO ----------------------------------------
 echo Creating Confluence Build Folder
 IF EXIST BUILD rmdir BUILD /S /Q
 md BUILD
 
-Echo .svn>exclude.txt
 Echo .git>exclude.txt
 Echo Thumbs.db>>exclude.txt
 Echo Desktop.ini>>exclude.txt
@@ -13,11 +14,11 @@ Echo exclude.txt>>exclude.txt
 
 ECHO ----------------------------------------
 ECHO Creating XPR File...
-START /B /WAIT ..\..\Tools\XBMCTex\dist\XBMCTex -input media -output media -noprotect
+START /B /WAIT XBMCTex -input media -output media -noprotect
 
 ECHO ----------------------------------------
 ECHO Copying XPR File...
-xcopy "media\Textures.xpr" "BUILD\skin.confluence\media\" /Q /I /Y
+xcopy "media\Textures.xpr" "BUILD\%ADDON_NAME_ID%\media\" /Q /I /Y
 
 ECHO ----------------------------------------
 ECHO Cleaning Up...
@@ -26,14 +27,27 @@ del "media\Textures.xpr"
 ECHO ----------------------------------------
 ECHO XPR Texture Files Created...
 ECHO Building Skin Directory...
-xcopy "fonts" "BUILD\skin.confluence\fonts" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "backgrounds" "BUILD\skin.confluence\backgrounds" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "resources" "BUILD\skin.confluence\resources" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "720p\*.*" "BUILD\skin.confluence\720p\" /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "colors\*.*" "BUILD\skin.confluence\colors\" /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "language" "BUILD\skin.confluence\language" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "fonts" "BUILD\%ADDON_NAME_ID%\fonts" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "backgrounds" "BUILD\%ADDON_NAME_ID%\backgrounds" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "resources" "BUILD\%ADDON_NAME_ID%\resources" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "720p\*.*" "BUILD\%ADDON_NAME_ID%\720p\" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "colors\*.*" "BUILD\%ADDON_NAME_ID%\colors\" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "language" "BUILD\%ADDON_NAME_ID%\language" /E /Q /I /Y /EXCLUDE:exclude.txt
 
 del exclude.txt
 
-copy *.xml "BUILD\skin.confluence\"
-copy *.txt "BUILD\skin.confluence\"
+copy *.xml "BUILD\%ADDON_NAME_ID%\"
+copy *.txt "BUILD\%ADDON_NAME_ID%\"
+
+ECHO ----------------------------------------
+ECHO Getting addon version...
+FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -NoProfile -Command ^
+    "[xml]$xml = Get-Content 'addon.xml'; $xml.addon.version"`) DO (
+    SET "VERSION=%%F"
+)
+
+ECHO ----------------------------------------
+ECHO Compressing addon...
+cd BUILD
+7z a -tzip "%ADDON_NAME_ID%-%VERSION%.zip" "%ADDON_NAME_ID%\*"
+move "%ADDON_NAME_ID%-%VERSION%.zip" "..\"
